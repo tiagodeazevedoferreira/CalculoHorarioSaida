719b0563cad2351b2f7feca72cc63be8c01b2892
tiagodeazevedoferreira <tiago.aferreira@gmail.com>
2025-04-25 13:59:55 -0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -4,55 +4,19 @@ const urlsToCache = [
   '/index.html',
   '/styles.css',
   '/script.js',
-  '/manifest.json',
-  '/icon.png',
-  '/tailwind.min.css'
+  '/icon.png'
 ];
 
 self.addEventListener('install', event => {
   event.waitUntil(
     caches.open(CACHE_NAME)
-      .then(cache => {
-        console.log('Cache aberto:', CACHE_NAME);
-        console.log('Cacheando arquivos:', urlsToCache);
-        return cache.addAll(urlsToCache);
-      })
-      .catch(err => console.error('Erro ao abrir cache:', err))
+      .then(cache => cache.addAll(urlsToCache))
   );
 });
 
 self.addEventListener('fetch', event => {
   event.respondWith(
-    caches.open(CACHE_NAME).then(cache => {
-      return caches.match(event.request).then(response => {
-        const fetchPromise = fetch(event.request).then(networkResponse => {
-          if (networkResponse && networkResponse.status === 200) {
-            cache.put(event.request, networkResponse.clone());
-            console.log('Atualizando cache:', event.request.url);
-          }
-          return networkResponse;
-        }).catch(err => {
-          console.error('Erro na rede:', err);
-          return response;
-        });
-        return response || fetchPromise;
-      });
-    })
-  );
-});
-
-self.addEventListener('activate', event => {
-  const cacheWhitelist = [CACHE_NAME];
-  event.waitUntil(
-    caches.keys().then(cacheNames => {
-      return Promise.all(
-        cacheNames.map(cacheName => {
-          if (!cacheWhitelist.includes(cacheName)) {
-            console.log('Deletando cache antigo:', cacheName);
-            return caches.delete(cacheName);
-          }
-        })
-      );
-    })
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
   );
 });
