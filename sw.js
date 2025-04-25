82c6d9876642aa00c4f6eb38202f24802570a55b
tiagodeazevedoferreira <tiago.aferreira@gmail.com>
2025-04-25 13:20:14 -0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -4,19 +4,51 @@ const urlsToCache = [
   '/index.html',
   '/styles.css',
   '/script.js',
-  '/icon.png'
+  '/manifest.json',
+  '/icon.png',
+  'https://cdn.tailwindcss.com'
 ];
 
 self.addEventListener('install', event => {
   event.waitUntil(
     caches.open(CACHE_NAME)
-      .then(cache => cache.addAll(urlsToCache))
+      .then(cache => {
+        console.log('Cache aberto:', CACHE_NAME);
+        console.log('Cacheando arquivos:', urlsToCache);
+        return cache.addAll(urlsToCache);
+      })
+      .catch(err => console.error('Erro ao abrir cache:', err))
   );
 });
 
 self.addEventListener('fetch', event => {
   event.respondWith(
     caches.match(event.request)
-      .then(response => response || fetch(event.request))
+      .then(response => {
+        if (response) {
+          console.log('Servindo do cache:', event.request.url);
+          return response;
+        }
+        console.log('Buscando da rede:', event.request.url);
+        return fetch(event.request).catch(err => {
+          console.error('Erro na rede:', err);
+        });
+      })
+  );
+});
+
+self.addEventListener('activate', event => {
+  const cacheWhitelist = [CACHE_NAME];
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (!cacheWhitelist.includes(cacheName)) {
+            console.log('Deletando cache antigo:', cacheName);
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
   );
 });
